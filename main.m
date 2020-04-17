0f753a25af0b05cb967705c1254be956576b7e75
Kamil Suchanek <kamilsuchanek95@gmail.com>
2020-04-18 01:18:53 +0200

    bbox with one face and additional artifacts, fixed
    fixed incorrectly detected faces, usually the largest ones are the right ones, so I take this as assumption and rejecting smaller sized frames. It is also assumed that there is one face in the painting.

--- main.m
+++ main.m
@@ -12,8 +12,15 @@ xml_face_detector = vision.CascadeObjectDetector('haarcascade_frontalface_defaul
 
 bbox = face_detector(my_image);
 % zwraca [x y wysokosc szerokosc] gdzie wys=szer, a x,y to lewy gorny rog.
-% if length(sum(bbox')) > 1
-% end
+if size(bbox, 1) > 1
+    max_size = max(bbox(:,4));
+    for i = 1:size(bbox, 1)
+        if bbox(i,4) == max_size
+            bbox = bbox(i, :);
+            break;
+        end
+    end
+end
 
 IFace = insertObjectAnnotation(my_image,'rectangle',bbox,'twarz');   
 figure
