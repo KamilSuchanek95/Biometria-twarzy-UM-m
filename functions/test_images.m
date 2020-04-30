2d78c50b1ae374709aac6b03d5d33cae0e15e7ef
Kamil Suchanek <kamilsuchanek95@gmail.com>
2020-04-30 12:19:24 +0200

    detect_face in test_images
    Fixed bugs in the code function "test_images" when calling the function detect_face and treatment of its output.
    Added correction for the case there is no detected face during the testing, the picture will not be counted in the statistics.

--- functions/test_images.m
+++ functions/test_images.m
@@ -10,31 +10,39 @@ function [scores] = test_images(Mdl, face_detector, path_of_test_images)
     test_folders = dir(path_of_test_images); 
     test_folders = test_folders(3:end);
     
-    for n_of_folders = 1:numel(test_folders) % loop for folders s1 s2...
-        test_images = dir(path_of_test_images + string(test_folders(n_of_folders).name)); 
+    cort = 0; % correction for no face detected
+    
+    for n_of_folder = 1:numel(test_folders) % loop for folders s1 s2...
+        test_images = dir(path_of_test_images + string(test_folders(n_of_folder).name)); 
         test_images = test_images(3:end);
         TP = 0;
-        disp("test image folder:  " + string(test_folders(n_of_folders).name));
-        for n_of_images = 1:numel(test_images) % loop for test images...
-            img_name = test_images(n_of_images).name;
-            img = imread(path_of_test_images + string(test_folders(n_of_folders).name) + "/" + string(img_name));
-            ROI = detect_face(img, face_detector);
+        disp('=============================================================');
+        disp("test image folder:  " + string(test_folders(n_of_folder).name));
+        for n_of_image = 1:numel(test_images) % loop for test images...
+            img_name = test_images(n_of_image).name;
+            img = imread(path_of_test_images + string(test_folders(n_of_folder).name) + "/" + string(img_name));
+            ROI = detect_face(img, face_detector, 0);
+            if isnan(ROI) 
+                fprintf('image: %s, failure! (no face) <=====\n', test_folders(n_of_folder).name + "/" + img_name);
+                cort = cort + 1;
+                continue
+            end
             if ~ismatrix(ROI) ROI = rgb2gray(ROI); end
             [label, score] = predict(Mdl, extractHOGFeatures(ROI));
             
-            scores{n_of_images,n_of_folders} = score;
+            scores{n_of_image,n_of_folder} = score;
             
-            if numel(strfind(string(test_folders(n_of_folders).name) + " ",string(label) + " ")) > 0
+            if numel(strfind(string(test_folders(n_of_folder).name) + " ",string(label) + " ")) > 0
                 TP = TP + 1; 
             else
                 disp('incorrect classification as ' + string(label) + ' on image: ' + string(img_name) + " score: " + string(max(score)))
             end
-            
             % disp('test ' + string(img_name) + ', detected as => ' + string(label) + 'with score: ' + string(max(score)))
             % disp('=====')
         end
-        disp(string(test_folders(n_of_folders).name) + " TP = " + TP + ...
-            " FN = " + string(numel(test_images)-TP) + " sensitivity = " + string(TP/numel(test_images)) + ...
-            " miss rate = " + string((numel(test_images)-TP)/numel(test_images)))
+        disp(string(test_folders(n_of_folder).name) + " TP = " + TP + ...
+            " FN = " + string(numel(test_images)-TP-cort) + " sensitivity = " + string(TP/(numel(test_images)-cort)) + ...
+            " miss rate = " + string((numel(test_images)-TP-cort)/(numel(test_images)-cort)))
+        cort = 0;
     end
 end
